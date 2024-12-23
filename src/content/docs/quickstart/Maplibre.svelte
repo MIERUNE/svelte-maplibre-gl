bb35bd548d5040090b014e93e7b5a5bee6175319
Kanahiro <mediterranean1769@gmail.com>
2024-12-23 17:24:01 +0900

    fix: typo

--- src/content/docs/quickstart/Maplibre.svelte
+++ src/content/docs/quickstart/Maplibre.svelte
@@ -2,5 +2,5 @@
 	import { MapLibre } from 'svelte-maplibre-gl';
 </script>
 
-<!-- Height must set! You can use TailwindCSS syntax for styling. -->
+<!-- Height must be set! You can use TailwindCSS syntax for styling. -->
 <MapLibre class="h-[540px]" style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json" />
