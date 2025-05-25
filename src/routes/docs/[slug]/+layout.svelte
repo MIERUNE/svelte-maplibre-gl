83b53fc81fc0becbda9ad3f74f8495c2a400d957
Taku Fukada <naninunenor@gmail.com>
2025-05-25 22:09:14 +0900

    shadcn-svelte: tailwind v4

--- src/routes/docs/[slug]/+layout.svelte
+++ src/routes/docs/[slug]/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="mx-auto grid gap-4 px-4 md:grid-cols-[210px_minmax(0,1fr)] md:px-8 xl:container">
-	<aside class="sticky top-[4.5rem] bottom-0 hidden h-[calc(100vh-4rem)] overflow-scroll py-10 md:block">
+	<aside class="sticky top-18 bottom-0 hidden h-[calc(100vh-4rem)] overflow-scroll py-10 md:block">
 		<Index name="Docs" {toc} />
 	</aside>
 	<main class="grid-cols-[1fr_210px]">
