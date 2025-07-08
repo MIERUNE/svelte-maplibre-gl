41524d3318d9d80c9962cec1029f4b3ceadeb13e
Taku Fukada <naninunenor@gmail.com>
2025-07-08 13:33:47 +0900

    chore: Update devDeps (#124)
    * update devDeps
    
    * fmt

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-
+import { searchForWorkspaceRoot } from 'vite';
 import svelteDocgenPlugin from 'vite-plugin-svelte-docgen';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [svelteDocgenPlugin(), tailwindcss(), sveltekit()],
+
+	server: {
+		fs: {
+			allow: [searchForWorkspaceRoot(process.cwd())]
+		}
+	},
 
 	resolve: {
 		alias: {
