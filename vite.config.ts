52d1577431144ba349e818ff6f688723a024cc89
Taku Fukada <naninunenor@gmail.com>
2025-07-08 13:26:13 +0900

    update devDeps

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vitest/config';
-import { sveltekit } from '@sveltejs/kit/vite';
-import tailwindcss from '@tailwindcss/vite';
-
-import svelteDocgenPlugin from 'vite-plugin-svelte-docgen';
+import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { searchForWorkspaceRoot } from "vite";
+import svelteDocgenPlugin from "vite-plugin-svelte-docgen";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	plugins: [svelteDocgenPlugin(), tailwindcss(), sveltekit()],
 
+	server: {
+		fs: {
+			allow: [searchForWorkspaceRoot(process.cwd())],
+		},
+	},
+
 	resolve: {
 		alias: {
-			path: 'pathe'
-		}
+			path: "pathe",
+		},
 	},
 
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+	},
 });
