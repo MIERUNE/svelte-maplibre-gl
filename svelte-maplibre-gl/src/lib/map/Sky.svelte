f031cbb0cbc52c640268afbfa31186b2d79e33b2
Taku Fukada <naninunenor@gmail.com>
2026-01-28 13:46:46 +0900

    chore(docs): Upgrade vite to 7.3.1 (#159)

--- svelte-maplibre-gl/src/lib/map/Sky.svelte
+++ svelte-maplibre-gl/src/lib/map/Sky.svelte
@@ -11,6 +11,7 @@
 	if (!mapCtx.map) throw new Error('Map instance is not initialized.');
 
 	$effect(() => {
+		// @ts-expect-error: ignore
 		mapCtx.userSky = $state.snapshot(spec) as maplibregl.SkySpecification;
 		mapCtx.waitForStyleLoaded((map) => {
 			map.setSky(mapCtx.userSky as maplibregl.SkySpecification);
